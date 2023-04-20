816b049a74bf0a75c9c07cf31b0071e9506cf474
Wayne Herridge <wsherridge@icloud.com>
2023-04-20 01:44:54 +0100

    prep 1 and 2 updated

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        // Core Requirement 1 and 3
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+
+        string letter;
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        // Core Requirement 2
+        Console.WriteLine($"You earned an letter grade of {letter}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. Better luck next time!");
+        }
+
+        
     }
 }
