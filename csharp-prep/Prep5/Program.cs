b4570cdbc8f8bbe43bccad6ad90d7c60ca060361
Wayne Herridge <wsherridge@icloud.com>
2023-04-24 23:24:24 +0100

    added code for prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+
+        // Calling Functions
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write($"Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write($"Please enter your favorite number: ");
+            string userNumber = Console.ReadLine();
+            int favoriteNumber = int.Parse(userNumber);
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int num)
+        {
+            int square = num * num;
+            return square;
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
     }
 }
