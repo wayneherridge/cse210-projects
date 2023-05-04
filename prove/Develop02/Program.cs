b1ccc52837d8984c20e3890270d12d2d9e586a19
Wayne Herridge <wsherridge@icloud.com>
2023-05-04 22:08:18 +0100

    learning activity w3

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Console.WriteLine("Hello Develop02 World!");
+
+        // What does the program do?
+        // What user inputs does it have?
+        // Writing prompts / Menu List / Answer Prompt Question
+        // What output does it produce?
+        // Display the entries (txt)
+        // How does the program end?
+        // Quit - are you sure?
+
+        // Program
+        // Journal
+        // - Display all
+        // Entry
+        // Save
+        // Load
+        // PromptGenerator
+
+
     }
 }
