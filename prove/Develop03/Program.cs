c32dc3deb0d649a655ed9cb6f7804d0ca0bfa406
Wayne Herridge <wsherridge@icloud.com>
2023-05-27 20:45:31 +0100

    Scripture Memorizer Code Complete

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,91 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Clear the console
+        Console.Clear();
+
+        Reference reference = new Reference();
+        reference.LoadRef();
+        Scripture scripture = new Scripture();
+        scripture.LoadScriptures();
+        Word word = new Word();
+
+        //  Show the welcome message
+        Console.WriteLine("\n-----------------------------------------");
+        Console.WriteLine("Welcome to The Scripture Memorizer Program!");
+        Console.WriteLine("-------------------------------------------\n");
+
+        int userChoice = 0;
+
+        while (userChoice != 3)
+        {
+            userChoice = UserChoice();
+
+            switch (userChoice)
+            {
+                case 1:
+                    reference.DisplayRef();
+
+                    break;
+                case 2:
+                    string script = scripture.RandomScripture();
+                    string ref1 = reference.GetRef(scripture);
+                    word.GetRenderedText(scripture);
+                    word.GetRenderedRef(scripture);
+
+                    while (word._hidden.Count < word._result.Length)
+                    {
+                        word.Show(ref1);
+                        word.GetReadKey();
+                    }
+                    word.Show(ref1);
+                    break;
+                case 3:
+                    Console.WriteLine("\n Come back to learn another verse soon! \n");
+                    break;
+                default:
+                    Console.WriteLine($"\nPlease enter 1,2 or Q!");
+                    break;
+            }
+        }
+    }
+
+    static int UserChoice()
+    // Method to display choices to user
+    {
+        Reference reference = new Reference();
+
+        string choices = $@"
+
+Please select one of the following options:
+1. Display all available scripture references
+2. Randomly select a scripture to work on
+Q. Quit
+
+What would you like to do?  ";
+
+        Console.Write(choices);
+
+        string userInput = Console.ReadLine();
+        userInput.ToLower();
+        int userChoice = 0;
+        try
+        {
+            if (userInput == "q")
+            {
+                userInput = "3";
+            }
+            userChoice = int.Parse(userInput);
+        }
+        catch (FormatException)
+        {
+            userChoice = 0;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(
+                $"Unexpected error:  {exception.Message}");
+        }
+        return userChoice;
     }
 }
---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,77 @@
+using System;
+
+public class Reference
+{
+    // Variables
+    public List<Reference> _reference = new List<Reference>();
+    private string _fileName = "Scriptures.txt";
+    private string _key;
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
+
+    // Methods
+    public void LoadRef()
+    {
+        List<string> readText = File.ReadAllLines(_fileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+
+        foreach (string line in readText)
+        {
+            string[] entries = line.Split(";");
+
+            Reference entry = new Reference();
+
+            entry._key = entries[0];
+            entry._book = entries[1];
+            entry._chapter = int.Parse(entries[2]);
+            entry._verseStart = int.Parse(entries[3]);
+            entry._verseEnd = int.Parse(entries[4]);
+
+            _reference.Add(entry);
+        }
+    }
+
+    public void DisplayRef()
+    {
+        foreach (Reference item in _reference)
+        {
+            // var test = item._verseEnd.Equals(0);
+            // Console.WriteLine(test);
+            if (item._verseEnd.Equals(0))
+            {
+                item.RefOne();
+            }
+            else
+            {
+                item.RefTwo();
+            }
+        }
+    }
+    public string GetRef(Scripture scripture)
+    {
+        var index = scripture._index;
+
+        var refi = _reference[index];
+        string ref1;
+        if (refi._verseEnd.Equals(0))
+        {
+            return ref1 = ($"\n{refi._book} {refi._chapter}:{refi._verseStart}");
+            
+        }
+        else
+        {
+            return ref1 = $"\n{refi._book} {refi._chapter}:{refi._verseStart}-{refi._verseEnd}";
+        }
+    }
+
+
+    public void RefOne()
+    {
+        Console.WriteLine($"\n{_book} {_chapter}:{_verseStart}");
+    }
+    public void RefTwo()
+    {
+        Console.WriteLine($"\n{_book} {_chapter}:{_verseStart}-{_verseEnd}");
+    }
+}
